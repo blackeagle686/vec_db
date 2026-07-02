d0a404e54e76bca2c11e1520f2b1894655f08d12
blackeagle686 <mathematecs1@gmail.com>
2026-07-02 13:22:50 +0300

    Update [routers.rs (impl App)]

--- src/api/routers.rs
+++ src/api/routers.rs
@@ -27,7 +27,7 @@ impl App {
         let app = Router::new()
             .route("/collection", post(create_collection))
             .route("/collection/:id", get(get_collection))
-            .route("/insert/collection/")
+            .route("/insert", post(insert_vector))
             .with_state(app_state);
         Self { app, app_state }
     }
