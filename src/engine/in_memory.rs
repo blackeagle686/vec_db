9199ed848e8e4781356d877941734160a0d73d16
blackeagle686 <mathematecs1@gmail.com>
2026-07-05 12:19:16 +0300

    Update [in_memory.rs]

--- src/engine/in_memory.rs
+++ src/engine/in_memory.rs
@@ -6,7 +6,7 @@ use std::fs;
 impl CollectionTrait for Collection{
     fn insert(
         &mut self, // MUST be &mut self, not mut self
-        id: &str,  // You forgot the ID parameter!
+        id: &str,  
         embeddings: Vec<f32>,
         max_layer: usize,
         metadata: Option<HashMap<String, String>>
