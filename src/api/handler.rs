39049911921ce8e5e79be6f99b1d942c5bdcbfc1
blackeagle686 <mathematecs1@gmail.com>
2026-07-05 13:21:18 +0300

    Update [handler.rs (pub async fn query_vector_handler)]

--- src/api/handler.rs
+++ src/api/handler.rs
@@ -79,10 +79,9 @@ pub async fn query_vector_handler(
     let mut engine = state.engine.write().unwrap();
     let collection = engine.get_collection_mut(&payload.collection_name).unwrap();
     let res = collection.query(payload.query_vector).unwrap();
-    let record = ; 
     Ok(Json(CollectionQueryResponse {
         vector_id: res.unwrap().0,
         distance: res.unwrap().1,
-        record: Some(record.clone()), 
+        record: Some(collection.get(&res.unwrap().0).unwrap()), 
     }))
 }
