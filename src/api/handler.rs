a087d76693aab04063fe75aca4c5a1360df4295b
blackeagle686 <mathematecs1@gmail.com>
2026-07-05 13:29:30 +0300

    Update [handler.rs (pub async fn query_vector_handler)]

--- src/api/handler.rs
+++ src/api/handler.rs
@@ -78,10 +78,10 @@ pub async fn query_vector_handler(
 ) -> Result<Json<CollectionQueryResponse>, (StatusCode, String) >{
     let mut engine = state.engine.write().unwrap();
     let collection = engine.get_collection_mut(&payload.collection_name).unwrap();
-    let (id, dist) = collection.query(payload.query_vector).unwrap().unwrap();
+    let res = collection.query(payload.query_vector).unwrap();
     Ok(Json(CollectionQueryResponse {
-        id: id,
-        distance: dist,
-        record: Some(collection.get(&id).unwrap().clone()), 
+        id: res.unwrap().0,
+        distance: res.unwrap().1,
+        record: Some(collection.get(&res.unwrap().0).unwrap().clone()), 
     }))
 }
