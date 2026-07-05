91533b9558e030b71464dae5a3976e7004fcf775
blackeagle686 <mathematecs1@gmail.com>
2026-07-05 12:47:25 +0300

    Update [in_memory.rs (impl CollectionTrait for Collection)]

--- src/engine/in_memory.rs
+++ src/engine/in_memory.rs
@@ -27,7 +27,8 @@ impl CollectionTrait for Collection{
     
     fn query(&self, query_vector: Vec<f32>) -> Result<Option<(String, f32)>, RecordError>{
         let mut index = HnswIndex::<CosineDistance>::new(self);
-        let mut res = Some(index.search(&query_vector).unwrap())
+        let mut res = Some(index.search(&query_vector).unwrap()); 
+        Ok
     }
 
     fn get(&self, id: &str) -> Result<&Record, RecordError>{
