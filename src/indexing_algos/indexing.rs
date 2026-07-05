edc6859251e5e296d112e51b5c419f17163fc4a5
blackeagle686 <mathematecs1@gmail.com>
2026-07-05 12:44:40 +0300

    Update [indexing.rs]

--- src/indexing_algos/indexing.rs
+++ src/indexing_algos/indexing.rs
@@ -2,7 +2,7 @@ use crate::domain::entities::Record;
 
 pub trait Indexing {
     fn insert(&mut self, record: Record);
-    fn search(&self, query: &[f32]) -> Option<(String, f32)>;
+    fn search(&self, query: &[f32]) -> Result<Option<(String, f32)>, RecordError>;
 }
 
 
