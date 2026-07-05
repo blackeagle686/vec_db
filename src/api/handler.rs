cd429b1717561a442da9e04f44d2eeb4be709c9a
blackeagle686 <mathematecs1@gmail.com>
2026-07-05 12:27:57 +0300

    Update [handler.rs (pub async fn insert_record_handler)]

--- src/api/handler.rs
+++ src/api/handler.rs
@@ -54,7 +54,7 @@ pub async fn insert_record_handler(
 ) -> Result<Json<DefaultSuccessCreationResponse>, (StatusCode, String)>{
     let mut engine = state.engine.write().unwrap();
     let mut collection = engine.get_collection(&payload.collection_name);
-    
+    collection.insert()
 
     
 
