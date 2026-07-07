c9bbf185b2a8f9f0362acb56bb2854e1d974b1b7
blackeagle686 <mathematecs1@gmail.com>
2026-07-07 14:15:46 +0300

    Update [hnsw.rs]

--- src/indexing_algos/hnsw.rs
+++ src/indexing_algos/hnsw.rs
@@ -2,8 +2,8 @@ use crate::domain::entities::{Collection, DistanceMetric, Record, RecordError, I
 use crate::indexing_algos::indexing::Indexing; 
 use std::marker::PhantomData;
 use rand::Rng;
-use std::sync::{Arc, Mutex, RwLock};
-
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 /*
     each collection must have one index
