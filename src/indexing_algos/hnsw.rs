1c13931bd8df7aa4fed835b016b8b36c2786705e
blackeagle686 <mathematecs1@gmail.com>
2026-07-07 14:14:41 +0300

    Update [hnsw.rs (impl<M: DistanceMetric> HnswIndex<M>)]

--- src/indexing_algos/hnsw.rs
+++ src/indexing_algos/hnsw.rs
@@ -3,7 +3,6 @@ use crate::indexing_algos::indexing::Indexing;
 use std::marker::PhantomData;
 use rand::Rng;
 use std::sync::{Arc, Mutex, RwLock};
-use std::marker::PhantomData;
 
 
 /*
@@ -96,8 +95,8 @@ impl<M: DistanceMetric> HnswIndex<M> {
     }
 
     fn search_layer(&self, query: &[f32], start_id: usize, layer: usize) -> (usize, f32) {
-        let index = self.index.read().await;
-        let collection = index.collection_ptr.read().await;
+        let index = self.index.read().unwrap();
+        let collection = index.collection_ptr.read().unwrap();
         let mut current = start_id;
         let mut curr_node = &collection.vectors[current];
         let mut best_dist = M::calculate(&curr_node.embeddings, query);
