4fc9eb78a0b06e0e82dfea22e772a5236e943664
blackeagle686 <mathematecs1@gmail.com>
2026-07-08 14:17:13 +0300

    Update [in_memory.rs]

--- src/engine/in_memory.rs
+++ src/engine/in_memory.rs
@@ -1,4 +1,8 @@
-use crate::{domain::{entities::{Collection, CollectionError, CollectionTrait, Engine, EngineError, EngineTrait, Index, Record, RecordError}, metrics::CosineDistance}, indexing_algos::{hnsw::HnswIndex, indexing::Indexing}};
+use crate::{domain::{entities::{Collection, CollectionError, CollectionTrait,
+     Engine, EngineError, EngineTrait, 
+     Index, Record, RecordError},
+      metrics::CosineDistance},
+       indexing_algos::{hnsw::HnswIndex, indexing::Indexing}};
 use std::collections::HashMap;
 use bincode;
 use std::fs; 
