8014e402439097c99f052f9d405f060b7a7e8345
blackeagle686 <mathematecs1@gmail.com>
2026-07-08 16:51:40 +0300

    Update [indexing.rs]

--- src/indexing_algos/indexing.rs
+++ src/indexing_algos/indexing.rs
@@ -2,5 +2,6 @@ use crate::domain::entities::{Collection, Record, RecordError};
 
 pub trait Indexing {
     fn insert(collection: &mut Collection, record: Record);
+    fn insert_batch(collection: &mut Collection, records: Vec<Record>);
     fn search(collection: &Collection, query: &[f32]) -> Result<Option<(String, f32)>, RecordError>;
 }
